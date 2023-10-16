75764db31805506412a36e06d940a4a9b5f660c6
Rchl04 <racheliaachmara@gmail.com>
2023-10-16 18:56:54 +0700

    menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+cout << "------------------------------------------------------------------" << endl;
+cout << "Nama : Rachelia Achmara" << endl << endl;
+cout << "NPM : 2310631170111" << endl << endl;
+cout << "Kelas : C - Informatika" << endl << endl;
+cout << "Cita-cita : Data Analyst" << endl << endl;
+cout << "Motivasi Masuk Informatika : Prospek kerja yang luas & menjanjikan" << endl;
+cout << "------------------------------------------------------------------" << endl;
+    return 0;
+}
